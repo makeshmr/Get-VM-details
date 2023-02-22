b01431e7d7f60c2dc8feb82f780ad1377cecb7fb
Makesh  Ramachandran <42498044+makeshmr@users.noreply.github.com>
2023-02-22 20:17:30 +0530

    Get-VM-details.ps1

---
+++ Get-VM-details.ps1
@@ -0,0 +1,16 @@
+﻿Function Get-VM-details {
+          Param (
+                     [Parameter(Mandatory = $True)]
+                     [Array]$VMList
+                  )
+                 foreach ($VM in $VMList){
+
+             Get-VM -Name $VM | Select Name,
+ @{N ='UUID';E={$_.ExtensionData.Config.Uuid}},
+ @{N ='GuestOS';E={$_.ExtensionData.Guest.GuestFullName}},
+ @{N ='HostName';E={$_.VMHost}},
+ @{N ='Cluster';E={$_.VMHost.Parent}},
+ @{N ='DataCenter'; E={Get-DataCenter}}
+              }
+ 
+             }
